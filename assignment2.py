f9468fde6803ffe9b8b9d9cca31f4089b2f3982d
moinqureshi2525 <138845323+moinqureshi2525@users.noreply.github.com>
2023-07-07 23:18:43 +0530

    Create assignment2.py

---
+++ assignment2.py
@@ -0,0 +1,67 @@
+# 1) Arithmatic operator
+a = 10
+b = 20
+c = None
+c = a + b
+print("The addition is:",c)
+c = a - b
+print("Subtraction is:",c)
+c = a * b
+print("Multiplication is:",c)
+c = a / b
+print("Division is:",c)
+c = a % b
+print("Exponent:",a**5)
+print("Floor division",a//b)
+
+
+# 2)Assignment Operator
+
+x = 20
+y = 30
+x +=5
+print("1st value of x ",x)
+y-=5
+print("Value of y ",y)
+x*=5
+print("The value of x is ",x)
+y/=3
+print("Value of y",y)
+x%=5
+print("Modulus of x is ",x)
+y//=2
+print("The value of y is ",y)
+
+
+# 3) Logical Operators
+
+print("And operation ",a and b)
+print("Or operation :",a or b)
+print("Not operation :",a!=b)
+
+
+#4) Comparison Operator
+
+print(">:",a>b)
+print("<",a<b)
+print("==",a==b)
+print("!=",a!=b)
+print("==",a==b)
+print("!=",a!=b)
+
+
+# 5)Bitwise Operator
+print("Bitwise &",a&b)
+print("Bitwise |",a|b)
+print("Bitwise <<",a<<b)
+print("Bitwise >>",a>>b)
+
+
+# 6)Identity Operator
+print("is :",a is b)
+print("is not :",a is not b)
+
+# 7)Membership Operator(special)
+v = "Moin"
+print('m' in v)
+print('q' not in v)
